d8d681739d3ac66d1da33d1a61e752d12adcd2ed
husky <huskyhacks.mk@gmail.com>
2023-09-07 15:05:57 -0400

    initial commit

---
+++ logformatter.go
@@ -0,0 +1,43 @@
+package logformatter
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+// LogType represents the type of log message.
+type LogType int
+
+const (
+	// Warning log type
+	Warning LogType = iota
+	// Info log type
+	Info
+	// Success log type
+	Success
+	// Error log type
+	Error
+)
+
+var logTypeColors = map[LogType]*color.Color{
+	Warning: color.New(color.FgYellow),
+	Info:    color.New(color.FgBlue),
+	Success: color.New(color.FgGreen),
+	Error:   color.New(color.FgRed),
+}
+
+var logTypeGlyphs = map[LogType]string{
+	Warning: "[!] ",
+	Info:    "[*] ",
+	Success: "[✔] ",
+	Error:   "[✘] ",
+}
+
+// Log prints a formatted log message with color and glyph based on the log type.
+func Log(logType LogType, message string) {
+	color := logTypeColors[logType]
+	glyph := logTypeGlyphs[logType]
+	formattedMessage := fmt.Sprintf("%s%s", color.Sprint(glyph), color.Sprint(message))
+	fmt.Println(formattedMessage)
+}
